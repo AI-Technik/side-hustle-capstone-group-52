c36e6b2dc474cba263f803e0572cc2150703f75f
Ibrahim Anifowoshe <iolawale301@gmail.com>
2022-04-23 23:39:09 +0100

    os fixed
    The os module has been fixed

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ const port = 5001;
 const server = http.createServer((req, res) => {
    res.setHeader("Content-Type", "text/html");
    res.setHeader("Access-Control-Allow-Origin", "*");
-  //  res.writeHead(200);
+  //  res.writeHead = 200;
   let path = "./frontend/";
   switch (req.url) {
     case "/":
       path += "index.html";
-      // res.statusCode(200)
+      // res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
-      // res.statusCode(200);
+      // res.statusCode = 200;
       break;
     default:
       path += "404.html";
-      // res.statusCode(404);
+      // res.statusCode = 404;
       break;
   }
 
  
   fs.readFile(path, (err, data) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead = 404;
       res.write("Error: File not found");
     } else {
       res.write(data);
@@ -38,15 +38,16 @@ const server = http.createServer((req, res) => {
   });
 
   const data = {
-    'hostname': 'os.hostname',
-    platform: 'os.platform',
-    architecture: 'os.arch',
-    numberOfCPUS: 'os.cpus',
-    networkInterfaces: 'os.networkInterfaces',
-    uptime: 'os.uptime'
+    'hostname': os.hostname(),
+    platform: os.platform(),
+    architecture: os.arch(),
+    numberOfCPUS: os.cpus(),
+    networkInterfaces: os.networkInterfaces(),
+    uptime: os.uptime()
   };
-  let newData = JSON.stringify(data)
-  fs.writeFile("sys.json", newData, (err, res) => {
+  fs.writeFile("sys.json", JSON.stringify(data), (err, res) => {
+    // res.statusCode(200);
+    // res.setHeader("Content-Type", "text/plain");
     if (err) {
       console.log(err);
     } else {
